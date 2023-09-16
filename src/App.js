42c8cc620c30cead7c10f2587d33f05d967f8502
Dinesh Suthar <suthardinesh626@gmail.com>
2023-09-16 14:43:16 +0530

    Home page is created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import ProductList from './features/product-list/ProductLists';
+import Home from './pages/Home';
 
 
 
 function App() {
   return (
     <div className="App">
-        <ProductList/>
+        <Home></Home>
     </div>
   );
 }
